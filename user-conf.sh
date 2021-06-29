701e7ad835dfc9ed675fa3f25116d22cf4d78370
Amila Upathissa <amilaupathissa@gmail.com>
2021-06-29 16:13:35 +0530

    change user script"

--- user-conf.sh
+++ user-conf.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+printf "This script will\n1. Add This user to Sudo group\n2. Enagle access through ssh to this user\n"
 read -p "Enter username : " USER_NAME
 cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
 if [ $? -eq 0 ] ; then
@@ -8,7 +9,6 @@ if [ $? -eq 0 ] ; then
     if [ $? -eq 0 ]; then
     	echo "User added to the sudo group complete"
     fi
-
     
     #resync ssh keys
     rsync --archive --chown=${USER_NAME}:${USER_NAME} ~/.ssh /home/${USER_NAME}
